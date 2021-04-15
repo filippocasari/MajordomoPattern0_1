f0d44680970dc4931feda15336678cdd37e90adb
Filippo <fili9941@gmail.com>
2021-04-15 15:43:07 +0200

    added sample with JSON client and worker

--- clientJSONsynchronous.c
+++ clientJSONsynchronous.c
@@ -1,4 +1,7 @@
 //
+// Created by Filippo
+//
+//
 // Created by filippo on 01/04/2021.
 // ASYNCHRONOUS CLIENT WITH JSON FORMAT PAYLOAD
 //
@@ -11,7 +14,7 @@
 #define BROKER_ENDPOINT  "tcp://192.168.1.7:5000"
 #define REQUEST "GET"
 
-#define NUM_OF_REQUEST 100
+#define NUM_OF_REQUEST 50
 
 #define TYPE_REQUEST 0 //kind of coffee you want to require
 
@@ -19,12 +22,15 @@ int calculating_delay(const long *timestamps_receiving, const long *timestamps_s
 
 long calculating_time_serialization(json_object *REQ);
 
+void receiving_task(long *timestamps_receiving, long *timestamps_sent,
+                    mdp_client_t *session2, int *count_rep, int *num_no_replies);
 
 void print_serialized_object(struct json_object *REQ);
 
 long create_JSON_object(struct json_object *REQ);
 
-void calculating_time_of_sending(const int64_t *start, const int64_t *end, long *array_of_sending_times, const int *index);
+void
+calculating_time_of_sending(const int64_t *start, const int64_t *end, long *array_of_sending_times, const int *index);
 
 void print_average_time_of_sending(const long *array_of_sending_times);
 
@@ -47,6 +53,8 @@ int main(int argc, char *argv[]) {
 
 
     int count; // number of request
+    int count_rep = 0;
+    int num_no_replies = 0;
     zmsg_t *reply = NULL; //reply client will receive
     zmsg_t *request = zmsg_new(); // request initialization
     int64_t start_execution_time; //start time to see processing time
@@ -56,14 +64,16 @@ int main(int argc, char *argv[]) {
     //start the time
 
 
-    start_execution_time=zclock_mono();
+    start_execution_time = zclock_mono();
     // send all request without wait the reply==> ZMQ doc calls this Asynchronous Client
     long timestamps_receiving[NUM_OF_REQUEST];
     long timestamps_sent[NUM_OF_REQUEST];
 
-
+    long int wait_at;
+    long int time_at;
     for (count = 0; count < NUM_OF_REQUEST; count++) {
-
+        time_at = zclock_mono();
+        wait_at = time_at + 1000;
         time_serialization_array[count] = create_JSON_object(REQ); //returns a time and create and serializes
         const char *string_request = json_object_to_json_string(REQ); //converting to a string
         printf("\nSTRING REQUEST: %s\n", string_request);
@@ -78,11 +88,22 @@ int main(int argc, char *argv[]) {
             puts("ERROR ");
         }
         //send request to broker for service "engine_1" in this case
-        int64_t start_time_sending =  zclock_usecs();
+        int64_t start_time_sending = zclock_usecs();
         mdp_client_send(session2, "engine_1", &request);
         int64_t end_time_sending = zclock_usecs();
         calculating_time_of_sending(&start_time_sending, &end_time_sending, time_sending_request, &count);
+        // -----------------------------------------------------------------------------------------------
 
+        receiving_task(timestamps_receiving, timestamps_sent, session2, &count_rep, &num_no_replies);
+
+        //------------------------------------------------------------------------------------------------
+
+        if ((long int) zclock_mono() < wait_at){
+
+            int time_difference= (int) ( wait_at - (long int) zclock_mono());
+            printf("TIME DIFFERENCE: %d\n", time_difference);
+            zclock_sleep(time_difference);
+        }
 
         // reinitialize request
         request = zmsg_new();
@@ -95,52 +116,8 @@ int main(int argc, char *argv[]) {
     zmsg_destroy(&reply);
 
 
-    int num_no_replies = 0;
-    int count_rep = 0;
-    //for loop to receive reply messages
-    for (; count_rep < NUM_OF_REQUEST; count_rep++) {
-        if (zctx_interrupted) {
-            zclock_log("error signal handled...");
-            break;
-        }
-        char *command; //command received
-        char *service; // from which service
-        zmsg_t *reply2 = mdp_client_recv(session2, &command, &service); //reply if any
-        timestamps_receiving[count_rep] = zclock_time();
 
 
-        if (reply2 == NULL) {
-            puts("NO REPLY...");
-            num_no_replies++;
-            continue;
-        }
-        char *reply_string = zmsg_popstr(reply2);
-        json_object *REP;
-        REP = json_tokener_parse(reply_string);
-        puts("REPLY = ");
-        long time_of_sending;
-        json_object_object_foreach(REP, key2, val2) {
-            printf("\t%s: %s\n", key2, json_object_to_json_string(val2));
-            char *string_value = strdup(json_object_to_json_string(val2));
-            char *key_str = strdup(key2);
-            char *ptr;
-            puts("");
-            if (strcmp(key_str, "timestamp") == 0) {
-                time_of_sending = strtol(string_value, &ptr, 10);
-                timestamps_sent[count_rep] = time_of_sending;
-                printf("Timestamp of captured receiving pack: %ld\n", timestamps_receiving[count_rep]);
-
-                long time_end_to_end = timestamps_receiving[count_rep] - time_of_sending;
-
-                printf("Time of delay end to end : \t%ld\n", time_end_to_end);
-
-            }
-
-        }
-        //if reply is null, just tell to stdout
-
-        zmsg_destroy(&reply2);
-    }
     // end time
     end = zclock_mono() - start_execution_time;
 
@@ -232,19 +209,65 @@ long calculating_time_serialization(struct json_object *REQ) {
     return time;
 }
 
-void calculating_time_of_sending(const int64_t *start, const int64_t *end, long *array_of_sending_times, const int *index) {
+void
+calculating_time_of_sending(const int64_t *start, const int64_t *end, long *array_of_sending_times, const int *index) {
 
-    array_of_sending_times[*index] =(long) (*end - *start);
+    array_of_sending_times[*index] = (long) (*end - *start);
     printf("TIME OF SENDING REQUEST: %ld [micro secs]\n", array_of_sending_times[*index]);
 
 }
 
-void print_average_time_of_sending(const long *array_of_sending_times){
-    long sum=0;
-    for(int i=0; i<NUM_OF_REQUEST; i++){
-        sum+=array_of_sending_times[i];
+void print_average_time_of_sending(const long *array_of_sending_times) {
+    long sum = 0;
+    for (int i = 0; i < NUM_OF_REQUEST; i++) {
+        sum += array_of_sending_times[i];
     }
-    long double average= (long double) sum/NUM_OF_REQUEST;
+    long double average = (long double) sum / NUM_OF_REQUEST;
 
     printf("AVERAGE TIME TO SEND A REQUEST: %Lf [micro seconds]\n", average);
+}
+
+void receiving_task(long *timestamps_receiving, long *timestamps_sent,
+                    mdp_client_t *session2, int *count_rep, int *num_no_replies) {
+
+    //no loop like Asynchronous, just receive if there is a reply
+    count_rep++;
+
+    char *command; //command received
+    char *service; // from which service
+    zmsg_t *reply2 = mdp_client_recv(session2, &command, &service); //reply if any
+    timestamps_receiving[*count_rep] = zclock_time();
+
+
+    if (reply2 == NULL) {
+        puts("NO REPLY...");
+        num_no_replies++;
+
+    } else {
+        char *reply_string = zmsg_popstr(reply2);
+        json_object *REP;
+        REP = json_tokener_parse(reply_string);
+        puts("REPLY = ");
+        long time_of_sending;
+        json_object_object_foreach(REP, key2, val2) {
+            printf("\t%s: %s\n", key2, json_object_to_json_string(val2));
+            char *string_value = strdup(json_object_to_json_string(val2));
+            char *key_str = strdup(key2);
+            char *ptr;
+            puts("");
+            if (strcmp(key_str, "timestamp") == 0) {
+                time_of_sending = strtol(string_value, &ptr, 10);
+                timestamps_sent[*count_rep] = time_of_sending;
+                printf("Timestamp of captured receiving pack: %ld\n", timestamps_receiving[*count_rep]);
+
+                long time_end_to_end = timestamps_receiving[*count_rep] - time_of_sending;
+
+                printf("Time of delay end to end : \t%ld\n", time_end_to_end);
+
+            }
+        }
+    }
+    //if reply is null, just tell to stdout
+    zmsg_destroy(&reply2);
+
 }
